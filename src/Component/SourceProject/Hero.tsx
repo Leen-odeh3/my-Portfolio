cb1bb3d4453322ec153123e2d3f94517f889463f
leen <leenodeh287@gmail.com>
2023-12-23 22:55:02 +0300

    Hero ProjectsPage

---
+++ src/Component/SourceProject/Hero.tsx
@@ -0,0 +1,14 @@
+
+
+const Hero = () => {
+  return (
+    <div style={{margin:'auto',width:"90%",textAlign:"center",paddingTop:"30px"}}>
+    <p style={{color:"rgb(127, 141, 170)",width:"60%",margin:"auto",fontSize:"22px",lineHeight:"30px"}}>  My projects makes use of vast variety of latest technology tools.
+       My best experience is to create ReactJS projects 
+      and deploy them to web applications using Netlify.</p>
+    </div>
+  )
+}
+
+export default Hero
+
--- src/Component/SourceProject/Project.tsx
+++ src/Component/SourceProject/Project.tsx
@@ -1,9 +1,10 @@
+import Hero from "./Hero"
 
 
 const Projects = () => {
   return (
     <div>
-      padajsfs
+    <Hero/>
     </div>
   )
 }
